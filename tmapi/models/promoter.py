464a2dbb45ba256f726986cd2442286ed235c99b
Caleb James <calebsjames@gmail.com>
2021-06-17 13:56:21 -0500

    build: contactNotes

--- tmapi/models/promoter.py
+++ tmapi/models/promoter.py
@@ -5,6 +5,7 @@ class Promoter(models.Model):
     address = models.CharField(max_length=100)
     cell_phone = models.CharField(max_length=15)
     city = models.CharField(max_length=100)
+    comments = models.CharField(max_length=500)
     company = models.CharField(max_length=100)
     email = models.CharField(max_length=75)
     name = models.CharField(max_length=50)
